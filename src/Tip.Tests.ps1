6110e3df57192e926fbcf9d5dc370c877fdbc6ef
deadlydog <deadlydog@hotmail.com>
2023-07-23 15:00:03 -0600

    Add Tip class and tests

---
+++ src/Tip.Tests.ps1
@@ -0,0 +1,70 @@
+using module .\Tip.ps1
+
+Describe 'Validating a Tip' {
+	Context 'Given a Tip has invalid properties' {
+		BeforeEach {
+			[Tip] $validTip = [Tip]::new()
+			$validTip.Id = "TipId"
+			$validTip.Title = 'Title of the tip'
+			$validTip.TipText = 'Tip Text'
+			$validTip.Example = 'Example'
+			$validTip.Urls = @('https://Url1.com', 'http://Url2.com')
+			$validTip.MinPowerShellVersion = [Version]::new(5, 1)
+			$validTip.Tags = @('Excel', 'Module')
+		}
+
+		It 'Should throw an error when no ID is supplied' {
+			[Tip] $tip = $validTip
+			$tip.Id = ''
+			{ $tip.Validate() } | Should -Throw
+		}
+
+		It 'Should throw an error when the ID contains spaces' {
+			[Tip] $tip = $validTip
+			$tip.Id = 'The ID should not contain spaces'
+			{ $tip.Validate() } | Should -Throw
+		}
+
+		It 'Should throw an error when no Title is supplied' {
+			[Tip] $tip = $validTip
+			$tip.Title = ''
+			{ $tip.Validate() } | Should -Throw
+		}
+
+		It 'Should throw an error when no TipText is supplied' {
+			[Tip] $tip = $validTip
+			$tip.TipText = ''
+			{ $tip.Validate() } | Should -Throw
+		}
+
+		It 'Should throw an error when more than 3 URLs are supplied' {
+			[Tip] $tip = $validTip
+			$tip.Urls = @('https://Url1.com', 'http://Url2.com', 'https://Url3.com', 'https://Url4.com')
+			{ $tip.Validate() } | Should -Throw
+		}
+
+		It 'Should throw an error when a URL does not start with http:// or https://' {
+			[Tip] $tip = $validTip
+			$tip.Urls = @('https://Url1.com', 'http://Url2.com', 'Url3.com')
+			{ $tip.Validate() } | Should -Throw
+		}
+	}
+
+	Context 'Given a Tip has all valid properties' {
+		BeforeEach {
+			[Tip] $validTip = [Tip]::new()
+			$validTip.Id = "TipId"
+			$validTip.Title = 'Title of the tip'
+			$validTip.TipText = 'Tip Text'
+			$validTip.Example = 'Example'
+			$validTip.Urls = @('https://Url1.com', 'http://Url2.com')
+			$validTip.MinPowerShellVersion = [Version]::new(5, 1)
+			$validTip.Tags = @('Excel', 'Module')
+		}
+
+		It 'Should not throw an error when all properties are valid' {
+			[Tip] $tip = $validTip
+			{ $tip.Validate() } | Should -Not -Throw
+		}
+	}
+}
--- src/Tip.ps1
+++ src/Tip.ps1
@@ -15,6 +15,11 @@ class Tip
 			throw [System.ArgumentException]::new('The Id property must be set.')
 		}
 
+		if ($this.Id.Contains(' '))
+		{
+			throw [System.ArgumentException]::new("The Id property value '$($this.Id)' cannot contain spaces. Use PascalCase.")
+		}
+
 		if ([string]::IsNullOrWhiteSpace($this.Title))
 		{
 			throw [System.ArgumentException]::new('The Title property must be set.')
@@ -29,9 +34,23 @@ class Tip
 		{
 			throw [System.ArgumentException]::new('You may only provide up to 3 Urls.')
 		}
+
+		foreach ($url in $this.Urls)
+		{
+			if ([string]::IsNullOrWhiteSpace($url))
+			{
+				throw [System.ArgumentException]::new('The Urls property must not contain null or empty values.')
+			}
+
+			if (-not ($url.StartsWith('http://') -or $url.StartsWith('https://')))
+			{
+				throw [System.ArgumentException]::new("The Urls property value '$url' must start with 'http://' or 'https://'.")
+			}
+		}
 	}
 }
 
+# TODO: Move enum to it's own file: https://stackoverflow.com/questions/69097932/include-enums-from-another-file
 enum Tags
 {
 	Excel
