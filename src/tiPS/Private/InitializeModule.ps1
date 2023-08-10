82e783c37838a139574b9dd55063e849e7171321
deadlydog <deadlydog@hotmail.com>
2023-08-10 14:38:32 -0600

    refactor: Extract code to function for readability and automatic scope cleanup

--- src/tiPS/Private/InitializeModule.ps1
+++ src/tiPS/Private/InitializeModule.ps1
@@ -1,18 +1,18 @@
-# Load all of the PowerShell Tips from the JSON file.
-[string] $moduleRootDirectoryPath = Split-Path -Path $PSScriptRoot -Parent
-[string] $powerShellTipsJsonFilePath = Join-Path -Path $moduleRootDirectoryPath -ChildPath 'PowerShellTips.json'
-[Tip[]] $tipObjects =
-	Get-Content -Path $powerShellTipsJsonFilePath -Raw |
-	ConvertFrom-Json
-
-[hashtable] $tipHashtable = [ordered]@{}
-foreach ($tip in $tipObjects)
+function LoadAllPowerShellTipsFromJsonFile
 {
-	$tipHashtable[$tip.Id] = $tip
+	[string] $moduleRootDirectoryPath = Split-Path -Path $PSScriptRoot -Parent
+	[string] $powerShellTipsJsonFilePath = Join-Path -Path $moduleRootDirectoryPath -ChildPath 'PowerShellTips.json'
+	[Tip[]] $tipObjects =
+		Get-Content -Path $powerShellTipsJsonFilePath -Raw |
+		ConvertFrom-Json
+
+	[hashtable] $tipHashtable = [ordered]@{}
+	foreach ($tip in $tipObjects)
+	{
+		$tipHashtable[$tip.Id] = $tip
+	}
+
+	# Scope our $Tips variable to the entire module so the Get-PowerShellTips cmdlet can access it.
+	New-Variable -Name 'Tips' -Value $tipHashtable -Option Constant -Scope Script
 }
-
-# Scope our $Tips variable to the entire module so the Get-PowerShellTips cmdlet can access it.
-New-Variable -Name 'Tips' -Value $tipHashtable -Option Constant -Scope Script
-
-# Clean up our temp variables to ensure they don't hold onto memory.
-Remove-Variable -Name 'moduleRootDirectoryPath', 'powerShellTipsJsonFilePath', 'tipObjects', 'tipHashtable'
+LoadAllPowerShellTipsFromJsonFile
