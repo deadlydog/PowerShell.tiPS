5c1fb10a03dcde3432e5aa9b5a63be15300068d8
deadlydog <deadlydog@hotmail.com>
2023-08-10 14:34:10 -0600

    refactor: Cleanup temp variables when done with them

--- src/tiPS/Private/InitializeModule.ps1
+++ src/tiPS/Private/InitializeModule.ps1
@@ -1,3 +1,4 @@
+# Load all of the PowerShell Tips from the JSON file.
 [string] $moduleRootDirectoryPath = Split-Path -Path $PSScriptRoot -Parent
 [string] $powerShellTipsJsonFilePath = Join-Path -Path $moduleRootDirectoryPath -ChildPath 'PowerShellTips.json'
 [Tip[]] $tipObjects =
@@ -12,3 +13,6 @@ foreach ($tip in $tipObjects)
 
 # Scope our $Tips variable to the entire module so the Get-PowerShellTips cmdlet can access it.
 New-Variable -Name 'Tips' -Value $tipHashtable -Option Constant -Scope Script
+
+# Clean up our temp variables to ensure they don't hold onto memory.
+Remove-Variable -Name 'moduleRootDirectoryPath', 'powerShellTipsJsonFilePath', 'tipObjects', 'tipHashtable'
