b85ba48091a8dd40210477485d10d30658a73a21
deadlydog <deadlydog@hotmail.com>
2023-08-13 01:29:32 -0600

    refactor: Remove code that was missed during migration

--- src/tiPS/tiPS.psm1
+++ src/tiPS/tiPS.psm1
@@ -96,11 +96,7 @@ enum Tags
 #Requires -Version 5.0
 Set-StrictMode -Version Latest
 
-# Import all of the Classes, Enums, and Private and Public functions from their respective directories.
-[string[]] $classFilePaths =
-	Get-ChildItem -Path $PSScriptRoot\Classes -Recurse -Filter '*.ps1' -Exclude '*.Tests.ps1' |
-	Select-Object -ExpandProperty FullName
-
+# Import all Private and Public functions from their respective directories.
 [string[]] $privateFunctionFilePaths =
 	Get-ChildItem -Path $PSScriptRoot\Private -Recurse -Filter '*.ps1' -Exclude '*.Tests.ps1' |
 	Select-Object -ExpandProperty FullName
@@ -109,7 +105,7 @@ Set-StrictMode -Version Latest
 	Get-ChildItem -Path $PSScriptRoot\Public -Recurse -Filter '*.ps1' -Exclude '*.Tests.ps1' |
 	Select-Object -ExpandProperty FullName
 
-[string[]] $functionFilesToImport = $classFilePaths + $privateFunctionFilePaths + $publicFunctionFilePaths
+[string[]] $functionFilesToImport = $privateFunctionFilePaths + $publicFunctionFilePaths
 
 $functionFilesToImport | ForEach-Object {
 	[string] $filePath = $_
