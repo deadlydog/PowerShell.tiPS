efcd2bbf64d2b6f3168968a7249284c108fd3a24
deadlydog <deadlydog@hotmail.com>
2023-08-20 13:12:48 -0600

    test: Ensure all tips are returned from the json file

--- src/tiPS/Public/Get-PowerShellTip.Tests.ps1
+++ src/tiPS/Public/Get-PowerShellTip.Tests.ps1
@@ -1,10 +1,36 @@
 using module .\..\tiPS.psm1
 
+BeforeAll {
+	[string] $PowerShellTipsJsonFilePath = Resolve-Path "$PSScriptRoot\..\PowerShellTips.json"
+}
+
 Describe 'Get-PowerShellTip' {
 	Context 'Given no parameters' {
 		It 'Should return a tip' {
 			$tip = Get-PowerShellTip
 			$tip | Should -Not -BeNullOrEmpty
+		}
+	}
+
+	Context 'Given a tip ID' {
+		It 'Should return the tip with the specified ID' {
+			$tip = Get-PowerShellTip -TipId 'PowerShellIsOpenSource'
+			$tip.Id | Should -Be 'PowerShellIsOpenSource'
+		}
+	}
+
+	Context 'Given the All switch' {
+		It 'Should return all tips' {
+			[int] $numberOfTipsInJsonFile =
+				Get-Content -Path $PowerShellTipsJsonFilePath |
+				ConvertFrom-Json |
+				Measure-Object |
+				Select-Object -ExpandProperty Count
+
+			$allTips = Get-PowerShellTip -All
+
+			$allTips | Should -Not -BeNullOrEmpty
+			$allTips.Count | Should -Be $numberOfTipsInJsonFile
 		}
 	}
 }
