2832882c48ff7851481942adbb1d3740cd242ec6
deadlydog <deadlydog@hotmail.com>
2023-08-23 13:18:51 -0600

    test: Add deployment smoke tests file

---
+++ deploy/Invoke-Smoke.Tests.ps1
@@ -0,0 +1,37 @@
+# These tests are runs as part of the deployment process to ensure the newly published module is working as expected.
+# These tests run against the installed module, not the source code, so they are more of a real-world test.
+
+BeforeAll {
+	Import-Module -Name tiPS -Force
+}
+
+Describe 'Get-PowerShellTip' {
+	Context 'Given no parameters' {
+		It 'Should return a tip' {
+			$tip = Get-PowerShellTip
+			$tip | Should -Not -BeNullOrEmpty
+		}
+	}
+
+	Context 'Given a valid tip ID' {
+		It 'Should return the tip with the specified ID' {
+			$tip = Get-PowerShellTip -TipId 'PowerShellIsOpenSource'
+			$tip.Id | Should -Be 'PowerShellIsOpenSource'
+		}
+	}
+
+	Context 'Given an invalid tip ID' {
+		It 'Should write an error' {
+			Get-PowerShellTip -TipId 'TipIdThatDoesNotExist' -ErrorVariable error -ErrorAction SilentlyContinue > $null
+			$error | Should -Not -BeNullOrEmpty
+		}
+	}
+
+	Context 'Given the All switch' {
+		It 'Should return all tips' {
+			$allTips = Get-PowerShellTip -All
+			$allTips | Should -Not -BeNullOrEmpty
+			$allTips.Count | Should -BeGreaterThan 2
+		}
+	}
+}
