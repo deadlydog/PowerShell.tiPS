fe7bf4b059db9264fd8cd377ec1391b2d6cfc44c
deadlydog <deadlydog@hotmail.com>
2023-08-26 11:51:31 -0600

    test: Add tests to ensure tip filenames are in the correct format

---
+++ src/PowerShellTipsDirectory.Tests.ps1
@@ -0,0 +1,14 @@
+Describe 'The files in the PowerShellTips directory' {
+	It 'Should have their file name in the correct format (yyyy-mm-dd-kebab-case-id.ps1)' {
+		[string] $fileNameRegex =
+		'^\d{4}-\d{2}-\d{2}' + # Starts with a date in the format yyyy-mm-dd.
+		'-' + # Followed by a hyphen.
+		'[a-z0-9-]+' + # Followed by any number of lower-case kebab-case words.
+		'\.ps1$' # Ends with the file extension .ps1.
+
+		$tipFiles = Get-ChildItem -Path "$PSScriptRoot\PowerShellTips" -File
+		$tipFiles | ForEach-Object {
+			$_.Name | Should -cMatch $fileNameRegex
+		}
+	}
+}
