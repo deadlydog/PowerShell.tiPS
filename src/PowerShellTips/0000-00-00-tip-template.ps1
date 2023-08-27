8d7ffd6997bcb38b37530bc1ffff41ae085e46a1
deadlydog <deadlydog@hotmail.com>
2023-08-27 10:38:14 -0600

    Upate PowerShellTip template a bit

--- src/PowerShellTips/0000-00-00-tip-template.ps1
+++ src/PowerShellTips/0000-00-00-tip-template.ps1
@@ -1,6 +1,6 @@
 $tip = [tiPS.PowerShellTip]::new()
 $tip.Id = 'AShortEasyToRememberIdWithNoSpaces'
-$tip.CreatedDate = [DateTime]::Parse('2023-07-15') # Use today's date.
+$tip.CreatedDate = [DateTime]::Parse('2023-07-15') # Change to today's date and remove this comment.
 $tip.Title = 'A brief title for the tip'
 $tip.TipText = @'
 A short description of the tip.
@@ -10,4 +10,4 @@ This can be multiple lines.
 $tip.Example = 'Example code to demonstrate the tip.'
 $tip.Urls = @('https://OneOrMoreUrls', 'https://ToLearnMoreAboutTheTip')
 $tip.MinPowerShellVersion = '0.0'
-$tip.Category = [tiPS.TipCategory]::Other
+$tip.Category = [tiPS.TipCategory]::Other # Community, Editor, Module, Syntax, Terminal, or Other
