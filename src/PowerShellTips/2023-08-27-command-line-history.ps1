c71b0f8d130ea07a2e7cb2afdef6d1e4c09c4d85
deadlydog <deadlydog@hotmail.com>
2023-08-27 10:38:40 -0600

    Update tip filename and put it in the proper category

--- src/PowerShellTips/2023-08-27-command-line-history.ps1
+++ src/PowerShellTips/2023-08-27-command-line-history.ps1
@@ -1,6 +1,6 @@
 $tip = [tiPS.PowerShellTip]::new()
 $tip.Id = 'CommandLineHistory'
-$tip.CreatedDate = [DateTime]::Parse('2023-08-28') # Use today's date.
+$tip.CreatedDate = [DateTime]::Parse('2023-08-28')
 $tip.Title = 'View your command line history'
 $tip.TipText = @'
 PowerShell lets you view your session history with `Get-History` and it's alias `h`.
@@ -9,4 +9,4 @@ PowerShell lets you view your session history with `Get-History` and it's alias 
 $tip.Example = 'Get-Content (Get-PSReadLineOption).HistorySavePath'
 $tip.Urls = @('https://learn.microsoft.com/powershell/module/psreadline/about/about_psreadline', 'https://learn.microsoft.com/powershell/module/microsoft.powershell.core/get-history')
 $tip.MinPowerShellVersion = '0.0'
-$tip.Category = [tiPS.TipCategory]::Other
+$tip.Category = [tiPS.TipCategory]::Terminal
