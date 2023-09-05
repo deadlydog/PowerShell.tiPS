ed61b9aa716c61c632cffc3e7849feae7eda2f88
deadlydog <deadlydog@hotmail.com>
2023-09-05 11:03:46 -0600

    feat: Allow setting just the AutomaticallyUpdateModule property in Set cmdlet

--- src/tiPS/Public/Set-TiPSConfiguration.ps1
+++ src/tiPS/Public/Set-TiPSConfiguration.ps1
@@ -1,14 +1,33 @@
 function Set-TiPSConfiguration
 {
-	[CmdletBinding(SupportsShouldProcess = $true)]
+	[CmdletBinding(SupportsShouldProcess = $true, DefaultParameterSetName = 'Default')]
 	Param
 	(
-		[tiPS.Configuration] $Configuration
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = 'ConfigurationObject')]
+		[tiPS.Configuration] $Configuration,
+
+		[Parameter(Mandatory = $false, ParameterSetName = 'Default')]
+		[tiPS.ModuleAutoUpdateCadence] $AutomaticallyUpdateModule = [tiPS.ModuleAutoUpdateCadence]::Never
 	)
 
-	if ($PSCmdlet.ShouldProcess('tiPS configuration', 'Set'))
+	# If the entire Configuration object is passed in, set it and return.
+	if ($PSBoundParameters.ContainsKey('Configuration'))
 	{
-		$script:TiPSConfiguration = $Configuration
-		WriteConfigurationToFile -Config $script:TiPSConfiguration
+		if ($PSCmdlet.ShouldProcess('tiPS configuration', 'Set'))
+		{
+			$script:TiPSConfiguration = $Configuration
+			WriteConfigurationToFile -Config $script:TiPSConfiguration
+		}
+		return
+	}
+
+	# If the ModuleAutoUpdateCadence is passed in, set it.
+	if ($PSBoundParameters.ContainsKey('AutomaticallyUpdateModule'))
+	{
+		if ($PSCmdlet.ShouldProcess('tiPS configuration AutomaticallyUpdateModule property', 'Set'))
+		{
+			$script:TiPSConfiguration.AutomaticallyUpdateModule = $AutomaticallyUpdateModule
+			WriteConfigurationToFile -Config $script:TiPSConfiguration
+		}
 	}
 }
