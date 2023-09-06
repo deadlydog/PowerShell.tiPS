15402a4daaba9e1d9a3cc1e653a3d125a00965b8
deadlydog <deadlydog@hotmail.com>
2023-09-06 09:14:56 -0600

    refactor: Use variables for Write tip colors

--- src/tiPS/Private/WritePowerShellTipToTerminal.ps1
+++ src/tiPS/Private/WritePowerShellTipToTerminal.ps1
@@ -8,6 +8,13 @@ function WritePowerShellTipToTerminal
 		[tiPS.PowerShellTip] $Tip
 	)
 
+	[ConsoleColor] $headerColor = [ConsoleColor]::Cyan
+	[ConsoleColor] $tipTextColor = [ConsoleColor]::White
+	[ConsoleColor] $exampleColor = [ConsoleColor]::Yellow
+	[ConsoleColor] $urlsColor = [ConsoleColor]::Green
+	[ConsoleColor] $minPowerShellVersionColor = [ConsoleColor]::Red
+
+
 	# Calculate how many header characters to put on each side of the title to make it look nice.
 	[int] $numberOfCharactersInHeader = 80
 	[int] $numberOfHeaderCharactersOnEachSideOfTitle =
@@ -22,27 +29,27 @@ function WritePowerShellTipToTerminal
 		('-' * $numberOfHeaderCharactersOnEachSideOfTitle) +
 		' ' + $Tip.Title + ' ' +
 		('-' * ($numberOfHeaderCharactersOnEachSideOfTitle + $additionalHeaderCharacterNeeded))
-	Write-Host $header -ForegroundColor Cyan
+	Write-Host $header -ForegroundColor $headerColor
 
-	Write-Host $Tip.TipText
+	Write-Host $Tip.TipText -ForegroundColor $tipTextColor
 
 	if (-not [string]::IsNullOrWhiteSpace($Tip.Example))
 	{
-		Write-Host 'Example: ' -ForegroundColor Yellow -NoNewline
-		Write-Host $Tip.Example -ForegroundColor Yellow
+		Write-Host 'Example: ' -ForegroundColor $exampleColor -NoNewline
+		Write-Host $Tip.Example -ForegroundColor $exampleColor
 	}
 
 	if ($Tip.UrlsAreProvided)
 	{
-		Write-Host 'More information: ' -ForegroundColor Green -NoNewline
-		Write-Host $Tip.Urls -ForegroundColor Green
+		Write-Host 'More information: ' -ForegroundColor $urlsColor -NoNewline
+		Write-Host $Tip.Urls -ForegroundColor $urlsColor
 	}
 
 	if ($Tip.MinPowerShellVersionIsProvided)
 	{
-		Write-Host 'Required PowerShell version or greater: ' -ForegroundColor Red -NoNewline
-		Write-Host $Tip.MinPowerShellVersion -ForegroundColor Red
+		Write-Host 'Required PowerShell version or greater: ' -ForegroundColor $minPowerShellVersionColor -NoNewline
+		Write-Host $Tip.MinPowerShellVersion -ForegroundColor $minPowerShellVersionColor
 	}
 
-	Write-Host ('-' * $numberOfCharactersInHeader) -ForegroundColor Cyan
+	Write-Host ('-' * $numberOfCharactersInHeader) -ForegroundColor $headerColor
 }
