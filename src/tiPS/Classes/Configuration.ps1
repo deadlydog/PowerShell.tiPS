65e42b5a8cdf2eec54120eea0f16b238d260818d
deadlydog <deadlydog@hotmail.com>
2023-09-07 17:22:13 -0600

    fix: Only load types if they have not already been loaded.

--- src/tiPS/Classes/Configuration.ps1
+++ src/tiPS/Classes/Configuration.ps1
@@ -1,5 +1,9 @@
 # Use C# classes and enums as they provide a better dev and module consumer experience:
 # 	https://blog.danskingdom.com/How-and-where-to-properly-define-classes-and-enums-in-your-PowerShell-modules/
 # Store the actual code in a .cs file to get syntax checking and IntelliSense, and just read it in and load it here.
-[string] $classDefinition = Get-Content -Path "$PSScriptRoot\Configuration.cs" -Raw
-Add-Type -Language CSharp -TypeDefinition $classDefinition
+# Only add the types if they don't already exist. This is quicker than re-adding the types and it failing.
+if (-not ('tiPS.Configuration' -as [type]))
+{
+	[string] $classDefinition = Get-Content -Path "$PSScriptRoot\Configuration.cs" -Raw
+	Add-Type -Language CSharp -TypeDefinition $classDefinition
+}
