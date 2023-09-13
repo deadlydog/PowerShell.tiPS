00a9980cfefdf582213103b6a77d5903372e03de
deadlydog <deadlydog@hotmail.com>
2023-09-13 16:01:59 -0600

    chore: Add script for building CSharp classes and copying DLL to the module's directory

---
+++ build/Build-CSharpClasses.ps1
@@ -0,0 +1,17 @@
+# This script builds the CSharp Classes DLL file and places it in the Classes directory of the module.
+
+[CmdletBinding()]
+Param()
+
+[string] $repositoryRoot = Split-Path $PSScriptRoot -Parent -Resolve
+[string] $moduleDirectoryPath = Join-Path -Path $repositoryRoot -ChildPath 'src/tiPS'
+[string] $moduleClassesDirectoryPath = Join-Path -Path $moduleDirectoryPath -ChildPath 'Classes'
+[string] $csharpSolutionDirectoryPath = Join-Path -Path $repositoryRoot -ChildPath 'src/CSharpClasses'
+[string] $csharpSlnFilePath = Join-Path -Path $csharpSolutionDirectoryPath -ChildPath 'tiPSClasses.sln'
+[string] $csharpClassesDllDirectoryPath = Join-Path -Path $csharpSolutionDirectoryPath -ChildPath 'tiPSClasses/bin/Release/netstandard2.0'
+
+Write-Output "Building C# sln in Release mode."
+& dotnet.exe build "$csharpSlnFilePath" --configuration Release
+
+Write-Output "Copying the DLL files in '$csharpClassesDllDirectoryPath' to the module's Classes directory '$moduleClassesDirectoryPath'."
+Copy-Item -Path "$csharpClassesDllDirectoryPath/*" -Destination $moduleClassesDirectoryPath -Include '*.dll' -Force
