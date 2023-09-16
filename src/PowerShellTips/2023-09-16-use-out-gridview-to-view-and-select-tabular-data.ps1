706aec8631c015d33809ab261a30cc8e37018f90
deadlydog <deadlydog@hotmail.com>
2023-09-16 11:42:27 -0600

    tip: Add Out-GridView tip

---
+++ src/PowerShellTips/2023-09-16-use-out-gridview-to-view-and-select-tabular-data.ps1
@@ -0,0 +1,12 @@
+$tip = [tiPS.PowerShellTip]::new()
+$tip.CreatedDate = [DateTime]::Parse('2023-09-16')
+$tip.Title = 'Use Out-GridView to view and select tabular data'
+$tip.TipText = @'
+Instead of piping data to Out-Table, use Out-GridView. This opens a window with the data in a grid view, allowing you to interactively sort and filter the data. If you provide the -PassThru parameter, you can select one or more rows and the selected data will be returned to the pipeline, allowing you to save it to a variable or pipe it to other commands.
+
+NOTE: Out-GridView is only available on Windows.
+'@
+$tip.Example = 'Get-Process | Out-GridView -PassThru -Title "Select processes to return" | Select-Object -Property ProcessName,Id'
+$tip.Urls = @('https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/out-gridview', 'https://woshub.com/using-out-gridview-table-powershell/')
+$tip.MinPowerShellVersion = '0.0'
+$tip.Category = [tiPS.TipCategory]::CoreCmdlet # Community, CoreCmdlet, Editor, Module, Syntax, Terminal, or Other.
