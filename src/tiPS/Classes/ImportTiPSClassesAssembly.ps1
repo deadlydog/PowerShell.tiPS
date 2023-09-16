eb40ed48d0a5f63ff1239e65cb05ceaae1b10c90
deadlydog <deadlydog@hotmail.com>
2023-09-16 12:42:56 -0600

    refactor: Remove unnecessary code after refactoring all Pester tests and Internal Tools to load the module instead of the classes dll directly

--- src/tiPS/Classes/ImportTiPSClassesAssembly.ps1
+++ src/tiPS/Classes/ImportTiPSClassesAssembly.ps1
@@ -1,15 +1,5 @@
 if (-not ('tiPS.PowerShellTip' -as [type]))
 {
-	# This script may be called from a Pester test or an internal tool, where the PSModuleRoot variable
-	# may not be set, so fallback to using PSScriptRoot in those cases.
-	[string] $assemblyFilePath = [string]::Empty
-	if (Test-Path -Path variable:script:PSModuleRoot)
-	{
-		$assemblyFilePath = Resolve-Path -Path "$script:PSModuleRoot/Classes/tiPSClasses.dll"
-	}
-	else
-	{
-		$assemblyFilePath = Resolve-Path -Path "$PSScriptRoot/tiPSClasses.dll"
-	}
+	$assemblyFilePath = Resolve-Path -Path "$script:PSModuleRoot/Classes/tiPSClasses.dll"
 	Add-Type -Path $assemblyFilePath
 }
