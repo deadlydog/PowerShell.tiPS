b32b157857ad4588ed5480afd99151b435e9fe43
deadlydog <deadlydog@hotmail.com>
2023-09-17 11:52:30 -0600

    refactor: Update warning message with new function name missed during refactoring

--- src/tiPS/Public/Set-TiPSConfiguration.ps1
+++ src/tiPS/Public/Set-TiPSConfiguration.ps1
@@ -106,7 +106,7 @@ function Set-TiPSConfiguration
 			[bool] $tiPSModuleIsImportedByPowerShellProfile = Test-PowerShellProfileImportsTiPS
 			if (-not $tiPSModuleIsImportedByPowerShellProfile)
 			{
-				Write-Warning "tiPS can only perform automatic actions when it is imported into the current PowerShell session. Run 'Edit-ProfileToImportTiPS' to update your PowerShell profile import tiPS automatically when a new session starts, or manually add 'Import-Module -Name tiPS' to your profile file. If you are importing the module in a different way, such as in a script that is dot-sourced into your profile, you can ignore this warning."
+				Write-Warning "tiPS can only perform automatic actions when it is imported into the current PowerShell session. Run 'Add-TiPSImportToPowerShellProfile' to update your PowerShell profile import tiPS automatically when a new session starts, or manually add 'Import-Module -Name tiPS' to your profile file. If you are importing the module in a different way, such as in a script that is dot-sourced into your profile, you can ignore this warning."
 			}
 		}
 	}
