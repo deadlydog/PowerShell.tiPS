41ae31fd1ead99e89746fbea8420e9df2c45e82f
deadlydog <deadlydog@hotmail.com>
2023-09-19 11:35:32 -0600

    test: Call function in module instead of dot-sourcing the private functions file

--- src/tiPS/Private/AutomaticWritePowerShellTipFunctions.Tests.ps1
+++ src/tiPS/Private/AutomaticWritePowerShellTipFunctions.Tests.ps1
@@ -1,98 +1,96 @@
 using module './../tiPS.psm1'
 
-BeforeAll {
-	. "$PSScriptRoot/AutomaticWritePowerShellTipFunctions.ps1"
-}
-
-Describe 'Calling WriteAutomaticPowerShellTipIfNeeded' {
-	BeforeEach {
-		Mock -CommandName GetLastAutomaticTipWrittenDateFilePath -MockWith {
-			# We have to use GetUnresolvedProviderPathFromPSPath because the File.ReadAllText method cannot read from the TestDrive provider, and we cannot use Resolve-Path because the file does not exist yet.
-			return $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath('TestDrive:/LastAutomaticTipWrittenDate.txt')
-		}
-		Mock -CommandName WriteAutomaticPowerShellTip -MockWith {} -Verifiable
-		Mock -CommandName TestPowerShellSessionIsInteractive -MockWith { return $true }
-	}
-
-	Context 'When the WritePowerShellTipCadence is Never' {
-		It 'Should not show a tip' {
-			$config = [tiPS.Configuration]::new()
-			$config.AutoWritePowerShellTipCadence = [tiPS.WritePowerShellTipCadence]::Never
-
-			WriteAutomaticPowerShellTipIfNeeded -Config $config
-
-			Assert-MockCalled WriteAutomaticPowerShellTip -Times 0 -Exactly
-		}
-	}
-
-	Context 'When the WritePowerShellTipCadence is EverySession' {
-		It 'Should show a tip' {
-			$config = [tiPS.Configuration]::new()
-			$config.AutoWritePowerShellTipCadence = [tiPS.WritePowerShellTipCadence]::EverySession
-			WriteLastAutomaticTipWrittenDate -LastAutomaticTipWrittenDate ([DateTime]::Now.Date.AddDays(-2))
-
-			WriteAutomaticPowerShellTipIfNeeded -Config $config
-
-			Assert-MockCalled WriteAutomaticPowerShellTip -Times 1 -Exactly
-		}
-	}
-
-	Context 'When the WritePowerShellTipCadence is Daily' {
-		It 'Should update the module if the last update was more than 1 day ago' {
-			$config = [tiPS.Configuration]::new()
-			$config.AutoWritePowerShellTipCadence = [tiPS.WritePowerShellTipCadence]::Daily
-			WriteLastAutomaticTipWrittenDate -LastAutomaticTipWrittenDate ([DateTime]::Now.Date.AddDays(-2))
-
-			WriteAutomaticPowerShellTipIfNeeded -Config $config
-
-			Assert-MockCalled WriteAutomaticPowerShellTip -Times 1 -Exactly
-		}
-
-		It 'Should not update the module if the last update was less than 1 day ago' {
-			$config = [tiPS.Configuration]::new()
-			$config.AutoWritePowerShellTipCadence = [tiPS.WritePowerShellTipCadence]::Daily
-			WriteLastAutomaticTipWrittenDate -LastAutomaticTipWrittenDate ([DateTime]::Now.Date)
-
-			WriteAutomaticPowerShellTipIfNeeded -Config $config
-
-			Assert-MockCalled WriteAutomaticPowerShellTip -Times 0 -Exactly
-		}
-	}
-
-	Context 'When the ShowPowerShellTipCandence is Weekly' {
-		It 'Should update the module if the last update was more than 7 days ago' {
-			$config = [tiPS.Configuration]::new()
-			$config.AutoWritePowerShellTipCadence = [tiPS.WritePowerShellTipCadence]::Weekly
-			WriteLastAutomaticTipWrittenDate -LastAutomaticTipWrittenDate ([DateTime]::Now.Date.AddDays(-8))
-
-			WriteAutomaticPowerShellTipIfNeeded -Config $config
-
-			Assert-MockCalled WriteAutomaticPowerShellTip -Times 1 -Exactly
-		}
-
-		It 'Should not update the module if the last update was less than 7 days ago' {
-			$config = [tiPS.Configuration]::new()
-			$config.AutoWritePowerShellTipCadence = [tiPS.WritePowerShellTipCadence]::Weekly
-			WriteLastAutomaticTipWrittenDate -LastAutomaticTipWrittenDate ([DateTime]::Now.Date.AddDays(-6))
-
-			WriteAutomaticPowerShellTipIfNeeded -Config $config
-
-			Assert-MockCalled WriteAutomaticPowerShellTip -Times 0 -Exactly
-		}
-	}
-
-	Context 'When the PowerShell session is not interactive' {
+InModuleScope tiPS { # Must use InModuleScope to call private functions of the module.
+	Describe 'Calling WriteAutomaticPowerShellTipIfNeeded' {
 		BeforeEach {
-			Mock -CommandName TestPowerShellSessionIsInteractive -MockWith { return $false }
+			Mock -CommandName GetLastAutomaticTipWrittenDateFilePath -MockWith {
+				# We have to use GetUnresolvedProviderPathFromPSPath because the File.ReadAllText method cannot read from the TestDrive provider, and we cannot use Resolve-Path because the file does not exist yet.
+				return $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath('TestDrive:/LastAutomaticTipWrittenDate.txt')
+			}
+			Mock -CommandName WriteAutomaticPowerShellTip -MockWith {} -Verifiable
+			Mock -CommandName TestPowerShellSessionIsInteractive -MockWith { return $true }
 		}
 
-		It 'Should not show a tip' {
-			$config = [tiPS.Configuration]::new()
-			$config.AutoWritePowerShellTipCadence = [tiPS.WritePowerShellTipCadence]::EverySession
+		Context 'When the WritePowerShellTipCadence is Never' {
+			It 'Should not show a tip' {
+				$config = [tiPS.Configuration]::new()
+				$config.AutoWritePowerShellTipCadence = [tiPS.WritePowerShellTipCadence]::Never
 
-			WriteAutomaticPowerShellTipIfNeeded -Config $config
+				WriteAutomaticPowerShellTipIfNeeded -Config $config
 
-			Assert-MockCalled WriteAutomaticPowerShellTip -Times 0 -Exactly
+				Assert-MockCalled WriteAutomaticPowerShellTip -Times 0 -Exactly
+			}
+		}
+
+		Context 'When the WritePowerShellTipCadence is EverySession' {
+			It 'Should show a tip' {
+				$config = [tiPS.Configuration]::new()
+				$config.AutoWritePowerShellTipCadence = [tiPS.WritePowerShellTipCadence]::EverySession
+				WriteLastAutomaticTipWrittenDate -LastAutomaticTipWrittenDate ([DateTime]::Now.Date.AddDays(-2))
+
+				WriteAutomaticPowerShellTipIfNeeded -Config $config
+
+				Assert-MockCalled WriteAutomaticPowerShellTip -Times 1 -Exactly
+			}
+		}
+
+		Context 'When the WritePowerShellTipCadence is Daily' {
+			It 'Should update the module if the last update was more than 1 day ago' {
+				$config = [tiPS.Configuration]::new()
+				$config.AutoWritePowerShellTipCadence = [tiPS.WritePowerShellTipCadence]::Daily
+				WriteLastAutomaticTipWrittenDate -LastAutomaticTipWrittenDate ([DateTime]::Now.Date.AddDays(-2))
+
+				WriteAutomaticPowerShellTipIfNeeded -Config $config
+
+				Assert-MockCalled WriteAutomaticPowerShellTip -Times 1 -Exactly
+			}
+
+			It 'Should not update the module if the last update was less than 1 day ago' {
+				$config = [tiPS.Configuration]::new()
+				$config.AutoWritePowerShellTipCadence = [tiPS.WritePowerShellTipCadence]::Daily
+				WriteLastAutomaticTipWrittenDate -LastAutomaticTipWrittenDate ([DateTime]::Now.Date)
+
+				WriteAutomaticPowerShellTipIfNeeded -Config $config
+
+				Assert-MockCalled WriteAutomaticPowerShellTip -Times 0 -Exactly
+			}
+		}
+
+		Context 'When the ShowPowerShellTipCandence is Weekly' {
+			It 'Should update the module if the last update was more than 7 days ago' {
+				$config = [tiPS.Configuration]::new()
+				$config.AutoWritePowerShellTipCadence = [tiPS.WritePowerShellTipCadence]::Weekly
+				WriteLastAutomaticTipWrittenDate -LastAutomaticTipWrittenDate ([DateTime]::Now.Date.AddDays(-8))
+
+				WriteAutomaticPowerShellTipIfNeeded -Config $config
+
+				Assert-MockCalled WriteAutomaticPowerShellTip -Times 1 -Exactly
+			}
+
+			It 'Should not update the module if the last update was less than 7 days ago' {
+				$config = [tiPS.Configuration]::new()
+				$config.AutoWritePowerShellTipCadence = [tiPS.WritePowerShellTipCadence]::Weekly
+				WriteLastAutomaticTipWrittenDate -LastAutomaticTipWrittenDate ([DateTime]::Now.Date.AddDays(-6))
+
+				WriteAutomaticPowerShellTipIfNeeded -Config $config
+
+				Assert-MockCalled WriteAutomaticPowerShellTip -Times 0 -Exactly
+			}
+		}
+
+		Context 'When the PowerShell session is not interactive' {
+			BeforeEach {
+				Mock -CommandName TestPowerShellSessionIsInteractive -MockWith { return $false }
+			}
+
+			It 'Should not show a tip' {
+				$config = [tiPS.Configuration]::new()
+				$config.AutoWritePowerShellTipCadence = [tiPS.WritePowerShellTipCadence]::EverySession
+
+				WriteAutomaticPowerShellTipIfNeeded -Config $config
+
+				Assert-MockCalled WriteAutomaticPowerShellTip -Times 0 -Exactly
+			}
 		}
 	}
 }
