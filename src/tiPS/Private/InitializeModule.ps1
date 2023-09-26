d86d5c3a844d729ec412caf41e4c433b83b42ab3
deadlydog <deadlydog@hotmail.com>
2023-09-26 09:37:45 -0600

    Allow modifying the tips in the Tips script variable

--- src/tiPS/Private/InitializeModule.ps1
+++ src/tiPS/Private/InitializeModule.ps1
@@ -6,7 +6,7 @@ function InitializeModule
 
 	Write-Debug 'Reading all tips from JSON file and storing them in a $Tips variable for access by other module functions.'
 	[hashtable] $tipHashTable = ReadAllPowerShellTipsFromJsonFile
-	New-Variable -Name 'Tips' -Value $tipHashtable -Option Constant -Scope Script
+	New-Variable -Name 'Tips' -Value $tipHashtable -Scope Script
 
 	Write-Debug 'Reading in configuration from JSON file and storing it in a $TiPSConfiguration variable for access by other module functions.'
 	[tiPS.Configuration] $config = ReadConfigurationFromFileOrDefault
