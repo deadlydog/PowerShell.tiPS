c5cb05f2f271aede30be3a66dee97fbf7e29c9b1
deadlydog <deadlydog@hotmail.com>
2023-09-29 18:40:06 -0600

    tip: Add tip to generate ValidateSet values dynamically

---
+++ src/PowerShellTips/2023-09-29-use-dynamic-values-in-validateset-while-maintaining-tab-completion.ps1
@@ -0,0 +1,30 @@
+$tip = [tiPS.PowerShellTip]::new()
+$tip.CreatedDate = [DateTime]::Parse('2023-09-29')
+$tip.Title = 'Use dynamic values in ValidateSet while maintaining tab completion'
+$tip.TipText = @'
+The ValidateSet attribute is a great way to limit the values that can be passed to a parameter, and it provides tab autocompletion for the parameter values. Typically if you wanted to use dynamic values you would use the ValidateScript attribute instead, but then you lose the tab autocompletion.
+
+A way to get the best of both worlds is to create a class that implements the System.Management.Automation.IValidateSetValuesGenerator interface. This interface has a single method, GetValidValues(), that returns an array of strings. You can put the logic to dynamically retrieve your allowed parameter values in the GetValidValues() method. You can then use the ValidateSet attribute with the type of your class, and the tab autocompletion will work as expected.
+'@
+$tip.Example = @'
+class AllowedParameterValues : System.Management.Automation.IValidateSetValuesGenerator {
+	[string[]] GetValidValues() {
+		# Populate these values dynamically by querying a database, reading a file, calling an API, etc.
+		$values = @('Value1', 'Value2', 'Value3')
+		return $values
+	}
+}
+
+function Test-ValidateSet {
+	param (
+		[ValidateSet([AllowedParameterValues])]
+		[string] $Value
+	)
+	return $Value
+}
+
+Test-ValidateSet -Value # Tab complete here to see the dynamic values.
+'@
+$tip.Urls = @()
+$tip.MinPowerShellVersion = '0.0'
+$tip.Category = [tiPS.TipCategory]::Syntax # Community, Editor, Module, NativeCmdlet, Performance, Syntax, Terminal, or Other.
