e3651de05909a935f7c4dc4874b010dfbaff0ff3
deadlydog <deadlydog@hotmail.com>
2023-11-10 12:19:43 -0600

    tip: Fix tip title and add more info to it

--- src/PowerShellTips/2023-10-14-create-new-objects-with-dynamic-properties-using-calculated-properties.ps1
+++ src/PowerShellTips/2023-10-14-create-new-objects-with-dynamic-properties-using-calculated-properties.ps1
@@ -1,10 +1,12 @@
 $tip = [tiPS.PowerShellTip]::new()
 $tip.CreatedDate = [DateTime]::Parse('2023-10-14')
-$tip.Title = 'Make convenience to objects by using calculated properties'
+$tip.Title = 'Create new objects with dynamic properties using calculated properties'
 $tip.TipText = @'
-PowerShell is a dynamic language, which means that you can add properties to objects on the fly. By using calculated properties, you can add properties to objects that are calculated from other properties on the object, or data from external sources.
+PowerShell is a dynamic language, which means that you can add properties to objects on the fly. By using calculated properties, you can add properties to output objects that are calculated from other properties on the input object, or data from external sources.
 
 The easiest way to add calculated properties is by using the `Select-Object` cmdlet to return a new object back with the additional properties. You provide a hashtable to the `-Property` parameter, with a `Name` key for the name of the new property and an `Expression` key for the value. You can add as many calculated properties as you want.
+
+The main difference between a calculated property and adding a property to an object with Add-Member is that calculated properties are added to a new output object, while Add-Member modifies the original object.
 '@
 $tip.Example = @'
 # Get the first process and add a single calculated property to show how long it has been running.
@@ -25,6 +27,9 @@ StartTime    : 10/14/2023 12:59:24 PM
 RunningTime  : 00:33:03.0717040
 WorkingSet   : 73527296
 WorkingSetMb : 70.12109375
+
+# Get all processes with all original properties, plus the new calculated property.
+> Get-Process | Select-Object -Property *,@{ Name = "RunningTime"; Expression = { (Get-Date) - $_.StartTime } }
 '@
 $tip.Urls = @(
 	'https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_calculated_properties'
