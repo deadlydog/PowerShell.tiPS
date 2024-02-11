4429cd40ef88ecfeed583f4a4dfc4192cdcf8463
deadlydog <deadlydog@hotmail.com>
2024-02-11 12:42:52 -0600

    tip: Add PowerShelldle game tip

---
+++ src/PowerShellTips/2024-02-11-play-powershelldle-to-test-and-expand-your-powershell-knowledge.ps1
@@ -0,0 +1,25 @@
+$tip = [tiPS.PowerShellTip]::new()
+$tip.CreatedDate = [DateTime]::Parse('2024-02-11')
+$tip.Title = 'Play PowerShelldle to test and expand your PowerShell knowledge'
+$tip.TipText = @'
+Head on over to https://powershelldle.com. PowerShelldle is a Wordle-like game where you guess a new PowerShell cmdlet each day.
+
+Play with your friends and colleagues. It's a fun way to test and expand your PowerShell knowledge.
+'@
+$tip.Example = ''
+$tip.Urls = @(
+	'https://powershelldle.com'
+)
+$tip.Category = [tiPS.TipCategory]::Community # Community, Editor, Module, NativeCmdlet, Performance, Security, Syntax, Terminal, or Other.
+#$tip.ExpiryDate = [DateTime]::Parse('2024-10-30') # Optional. If the tip is not relevant after a certain date, set the expiration date. e.g. Announcing a conference or event.
+
+# Category meanings:
+# Community: Social events and community resources. e.g. PowerShell Summit, podcasts, etc.
+# Editor: Editor tips and extensions. e.g. VSCode, ISE, etc.
+# Module: Modules and module tips. e.g. PSScriptAnalyzer, Pester, etc.
+# NativeCmdlet: Native cmdlet tips. e.g. Get-Process, Get-ChildItem, Get-Content, etc.
+# Performance: Tips to improve runtime performance. e.g. foreach vs ForEach-Object, ForEach-Object -Parallel, etc.
+# Security: Security tips. e.g. ExecutionPolicy, Constrained Language Mode, passwords, etc.
+# Syntax: Syntax tips. e.g. splatting, pipeline, etc.
+# Terminal: Terminal shortcuts and tips. e.g. PSReadLine, Windows Terminal, ConEmu, etc.
+# Other: Tips that don't fit into any of the other categories.
