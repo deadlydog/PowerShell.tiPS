d041ab80714aac4ab88c25adac181a1cb0e52429
deadlydog <deadlydog@hotmail.com>
2024-02-22 20:29:47 -0600

    refactor: Update comment

--- deploy/Invoke-SmokeTests.ps1
+++ deploy/Invoke-SmokeTests.ps1
@@ -1,7 +1,7 @@
 # These tests are runs as part of the deployment process to ensure the newly published module is working as expected.
 # These tests run against the installed module, not the source code, so they are a real-world test and should not use mocks.
 # Since mocks are not used, we must be careful to not rely on state stored on the machine, such as the module Configuration file.
-# This is a great place to put tests that differ between operating systems, since they will be ran on all platforms.
+# This is a great place to put tests that differ between operating systems, since they will be ran on multiple platforms.
 # To run these tests on your local machine, see the comments in the BeforeAll block.
 
 BeforeAll {
